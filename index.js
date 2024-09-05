fa193cb755ea6024c50f092579af3ae25a4e35e9
21MoududAhammed <21moudud@gmail.com>
2024-09-05 23:02:49 +0600

    Initial setup for test the server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
